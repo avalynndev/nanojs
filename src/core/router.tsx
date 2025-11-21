6d03b2f05c7f74afcc25e70a5ae28ae878d66490
Avalynn <avalynndev@gmail.com>
2025-11-21 21:00:36 +0530

    better 404 page

--- src/core/router.tsx
+++ src/core/router.tsx
@@ -51,7 +51,48 @@ export function RouterProvider() {
 
   const route = globalRoutes.find((r) => r.path === currentPath);
 
-  if (!route) return <div>404 Not Found</div>;
+  if (!route)
+    return (
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          minHeight: "100vh",
+          backgroundColor: "#fff",
+          color: "#000",
+          padding: "20px",
+          flexDirection: "row",
+        }}
+      >
+        <h1
+          style={{
+            display: "inline-block",
+            margin: "0 20px 0 0",
+            padding: "0 23px 0 0",
+            fontSize: "24px",
+            fontWeight: 500,
+            verticalAlign: "top",
+            lineHeight: "49px",
+            borderRight: "1px solid rgba(0,0,0,0.3)",
+          }}
+        >
+          404
+        </h1>
+        <div style={{ display: "inline-block" }}>
+          <h2
+            style={{
+              fontSize: "14px",
+              fontWeight: 400,
+              lineHeight: "49px",
+              margin: 0,
+            }}
+          >
+            This page could not be found.
+          </h2>
+        </div>
+      </div>
+    );
 
   const Page = route.component;
   const Layout = route.layout ?? React.Fragment;
